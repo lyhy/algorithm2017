19b537672ecbe995078effcc689704a69796c519
Jun Wang <junwang@zingbox.com>
2017-11-12 07:19:22 -0800

    non recursive nested list weight; js overwirte

--- leetcode/js/zz-blocked-questions/nestedListWeightSum/nested-list-weight-sum.js
+++ leetcode/js/zz-blocked-questions/nestedListWeightSum/nested-list-weight-sum.js
@@ -1,22 +1,53 @@
+//var depthSum = function(arr){
+//    var result =  helpFun(arr, 1);
+//    console.log(result);
+//}
+//
+//var helpFun = function(arr, depth) {
+//    if(!arr || (Array.isArray(arr) && !arr.length))return 0;
+//
+//    var total = 0;
+//    for(var i = 0, len = arr.length; i < len; i++){
+//        if(typeof arr[i] === 'number')total += depth * arr[i];
+//        else
+//            total += helpFun(arr[i], depth+1);
+//    }
+//
+//    return total;
+//}
+
+//non-recursive
 var depthSum = function(arr){
-    var result =  helpFun(arr, 1);
-    console.log(result);
-}
+    var ret = 0;
+    var queque = [];
+    var depth = [];
 
-var helpFun = function(arr, depth) {
-    if(!arr || (Array.isArray(arr) && !arr.length))return 0;
-
-    var total = 0;
+    if(!Array.isArray(arr) || !arr.length)return ret;
     for(var i = 0, len = arr.length; i < len; i++){
-        if(typeof arr[i] === 'number')total += depth * arr[i];
-        else
-            total += helpFun(arr[i], depth+1);
+        queque.push(arr[i]);
+        depth.push(1);
     }
 
-    return total;
+    while(queque.length){
+        var top = queque.shift();
+        var deep = depth.shift();
+
+        if(typeof top ==='number'){
+            ret += top * deep;
+        }else{
+            for(var j = 0, lenj = top.length; j < lenj; j++){
+                queque.push(top[j]);
+                depth.push(deep+1);
+            }
+        }
+    }
+
+    return ret;
 }
+
+
 
 var input = [[1,1],2,[1,1]];
 var output = 10;
-depthSum(input)
+console.log(depthSum(input))
 module.exports = depthSum;
