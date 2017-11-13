1057c0c54c266ca499effd174f03ebc27a449a2d
Jun Wang <junwang@zingbox.com>
2017-11-12 21:16:56 -0800

    valid number added

--- leetcode/js/validNumber/valid-number.js
+++ leetcode/js/validNumber/valid-number.js
@@ -3,5 +3,32 @@
  * @return {boolean}
  */
 var isNumber = function(s) {
-    
+    s = s.trim();
+    var hasDot = false;
+    var hasE = false;
+    var hasNum = false;
+    var hasNumAfterE = true;
+
+    for(var i = 0, len = s.length; i < len; i++){
+        var char = s[i];
+        if(/[0-9]/.test(char)){
+            hasNum = true;
+            hasNumAfterE = true;
+        }else if(char === '.'){
+            if(hasE || hasDot)return false;
+            hasDot = true;
+        }else if(char === 'e'){
+            if(hasE || !hasNum)return false;
+            hasNumAfterE = false;
+            hasE = true;
+        }else if(char === '-' || char === '+') {
+            if(i !== 0 && s.charAt(i-1)!== 'e')return false;
+        }else {
+            return false;
+        }
+    }
+
+    return hasNum && hasNumAfterE;
 };
+
+module.exports = isNumber;
--- leetcode/js/validNumber/valid-number.test.js
+++ leetcode/js/validNumber/valid-number.test.js
@@ -3,7 +3,12 @@ jest.dontMock('./valid-number');
 describe('valid-number', function(){
     var func = require('./valid-number');
     it('valid-number', function(){
-        expect(func()).toEqual(true);
+        var input = '0', output = true;
+        //var input = '   0.1  ', output = true;
+        //var input = 'abc', output = false;
+        //var input = '1 a', output = false;
+        //var input = '2e10', output = true;
+        expect(func(input)).toEqual(true);
 
     });
 });
