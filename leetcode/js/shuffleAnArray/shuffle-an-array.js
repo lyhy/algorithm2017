77b9d683c1cc31bf7aa6cc8004a220e1297ea1f1
Jun Wang <junwang@zingbox.com>
2017-11-14 08:28:27 -0800

    shuffle still has issue

--- leetcode/js/shuffleAnArray/shuffle-an-array.js
+++ leetcode/js/shuffleAnArray/shuffle-an-array.js
@@ -2,15 +2,32 @@
  * @param {number[]} nums
  */
 var Solution = function(nums) {
-    
+    var ret = [];
+    permutationDFS(nums, 0, ret);
+    this.permutations = ret;
 };
+
+var permutationDFS = function(nums, start, ret) {
+    if(start >= nums.length) ret.push(nums.slice());
+    for(var i = start, len = nums.length; i < len; i++) {
+        swap(nums, start, i);
+        permutationDFS(nums, start + 1, ret);
+        swap(nums, start, i);
+    }
+}
+
+var swap = function(nums, i, j) {
+    var temp = nums[i];
+    nums[i] = nums[j];
+    nums[j] = temp;
+}
 
 /**
  * Resets the array to its original configuration and return it.
  * @return {number[]}
  */
 Solution.prototype.reset = function() {
-    
+    return this.permutations[0];
 };
 
 /**
@@ -18,7 +35,9 @@ Solution.prototype.reset = function() {
  * @return {number[]}
  */
 Solution.prototype.shuffle = function() {
-    
+    let min = 0, max = this.permutations.length;
+    var random = Math.floor(Math.random() * (max - min)) + min;
+    return this.permutations[random];
 };
 
 /** 
@@ -26,4 +45,17 @@ Solution.prototype.shuffle = function() {
  * var obj = Object.create(Solution).createNew(nums)
  * var param_1 = obj.reset()
  * var param_2 = obj.shuffle()
+ */
+var nums = [1,2,3]
+var obj = new Solution(nums);
+console.log(obj.permutations);
+console.log(obj.reset());
+console.log(obj.shuffle());
+
+
+/**
+ * Time exceed
+ * Last executed input:
+ ["Solution","shuffle"]
+ [[[1,2,3,4,5,6,7,8,9,10,11,12]],[]]
  */
