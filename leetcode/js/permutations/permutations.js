9b3705617410be3aea0cf010a7394cac38803694
Jun Wang <junwang@zingbox.com>
2017-11-13 17:25:57 -0800

    permutation added

--- leetcode/js/permutations/permutations.js
+++ leetcode/js/permutations/permutations.js
@@ -3,5 +3,41 @@
  * @return {number[][]}
  */
 var permute = function(nums) {
-    
+    var ret = [];
+
+    permDFS(nums, 0, ret)
+
+    return ret;
+
 };
+
+var permDFS = function(nums, start, ret) {
+    if(start >= nums.length)ret.push(nums.slice());
+    for(var i = start, len = nums.length; i < len; i++){
+        swap(nums, start, i);
+        permDFS(nums, start + 1, ret);
+        swap(nums, start, i);
+
+    }
+}
+
+var swap = function(nums, i, j){
+    var temp = nums[i];
+    nums[i] = nums[j];
+    nums[j] = temp;
+}
+
+
+var input = [1,2,3],
+    output = [
+        [1,2,3],
+        [1,3,2],
+        [2,1,3],
+        [2,3,1],
+        [3,1,2],
+        [3,2,1]
+    ];
+
+console.log(permute(input))
+
+module.exports = permute;
--- leetcode/js/permutations/permutations.test.js
+++ leetcode/js/permutations/permutations.test.js
@@ -3,7 +3,16 @@ jest.dontMock('./permutations');
 describe('permutations', function(){
     var func = require('./permutations');
     it('permutations', function(){
-        expect(func()).toEqual(true);
+        var input = [1,2,3],
+            output = [
+                [1,2,3],
+                [1,3,2],
+                [2,1,3],
+                [2,3,1],
+                [3,1,2],
+                [3,2,1]
+            ];
+        expect(func(input)).toEqual(output);
 
     });
 });
