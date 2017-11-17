e00bf772bea13726800ca63156d6a4cfd116d15b
Jun Wang <junwang@zingbox.com>
2017-11-17 09:22:03 -0800

    leetcode roman-to-integer added

--- leetcode/js/romanToInteger/roman-to-integer.js
+++ leetcode/js/romanToInteger/roman-to-integer.js
@@ -3,5 +3,27 @@
  * @return {number}
  */
 var romanToInt = function(s) {
-    
+    if(!s)return 0;
+    var prev = 0, curr, ret = 0,char;
+    for(var len = s.length, i = len -1; i >=0; i--){
+        char = s[i];
+        switch(char){
+            case 'I': curr = 1; break;
+            case 'V': curr = 5; break;
+            case 'X': curr = 10; break;
+            case 'L': curr = 50; break;
+            case 'C': curr = 100; break;
+            case 'D': curr = 500; break;
+            case 'M': curr = 1000; break;
+        }
+
+        ret = curr >= prev ? ret + curr : ret - curr;
+        prev = curr;
+    }
+
+    return ret;
+
+
 };
+
+module.exports = romanToInt;
--- leetcode/js/romanToInteger/roman-to-integer.test.js
+++ leetcode/js/romanToInteger/roman-to-integer.test.js
@@ -3,7 +3,10 @@ jest.dontMock('./roman-to-integer');
 describe('roman-to-integer', function(){
     var func = require('./roman-to-integer');
     it('roman-to-integer', function(){
-        expect(func()).toEqual(true);
+        var str ='LXXXVI', val =86;
+        //var str ='MDCC', val =1700;
+        //var str ='IV', val =4;
+        expect(func(str)).toEqual(val);
 
     });
 });
