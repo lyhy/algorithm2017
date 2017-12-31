51bf785fc780cc8a67f85748de200b0dff1ca000
Jun Wang <junwang@zingbox.com>
2017-12-31 06:38:10 -0800

    update

--- leetcode/js/burstBalloons/burst-balloons.js
+++ leetcode/js/burstBalloons/burst-balloons.js
@@ -1,7 +1,43 @@
+//https://www.youtube.com/watch?v=IFNibRVgFBo
 /**
  * @param {number[]} nums
  * @return {number}
  */
 var maxCoins = function(nums) {
-    
+    if(!nums || !nums.length)return 0;
+    var c = new Array(nums.length);
+
+    for(var p = 0; p < nums.length; p++){
+        c[p] = new Array(nums.length).fill(0);
+    }
+
+    for(var len = 1; len <= nums.length; len++){
+        for(var i = 0; i <= nums.length -len; i++){
+            var j = i + len -1;
+
+            for(var k = i; k <=j; k++){
+                var leftValue = 1;
+                var rightValue = 1;
+                if(i!=0)leftValue = nums[i-1];
+                if(j!=nums.length -1)rightValue = nums[j+1];
+
+                var before = 0;
+                var after = 0;
+                if(i!=k)before = c[i][k-1];
+                if(j!=k)after = c[k+1][j];
+
+                c[i][j]=Math.max(c[i][j],
+                leftValue * nums[k]*rightValue + before + after);
+
+            }
+        }
+    }
+
+    return c[0][nums.length-1];
 };
+
+var nums = [3, 1, 5, 8], output = 167;
+
+console.log(maxCoins(nums));
+
+
--- leetcode/js/islandPerimeter/island-perimeter.js
+++ leetcode/js/islandPerimeter/island-perimeter.js
@@ -3,5 +3,30 @@
  * @return {number}
  */
 var islandPerimeter = function(grid) {
-    
+    if(!grid || !grid.length)return 0;
+    var box = 0, nb = 0, m = grid.length, n = grid[0].length || 0;
+
+    for(var i = 0; i < m; i++){
+        for(var j = 0; j < n; j++){
+            if(grid[i][j]){
+                box++;
+                if(i > 0 && grid[i - 1][j])nb++;
+                if(i < m-1 && grid[i + 1][j])nb++;
+                if(j > 0 && grid[i][j-1])nb++;
+                if(j < n -1 && grid[i][j +1])nb++;
+            }
+        }
+
+
+    }
+
+    return box*4 -nb;
 };
+
+
+var a = [[0,1,0,0],
+    [1,1,1,0],
+    [0,1,0,0],
+    [1,1,0,0]], output = 16;
+
+console.log(islandPerimeter(a));
